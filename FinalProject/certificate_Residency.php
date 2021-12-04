581aba479230c09928c2b2b3b880f22cc23f62bd
jcorteeeeez <83940812+jcorteeeeez@users.noreply.github.com>
2021-12-04 10:07:25 +0800

    A draft of certificate of residency page

---
+++ FinalProject/certificate_Residency.php
@@ -0,0 +1,59 @@
+<?php
+    include 'header.php';
+    include 'navbar.php';
+?>
+<section class="au-breadcrumb m-t-75">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="au-breadcrumb-content">
+                                    <div class="au-breadcrumb-left">
+                                        <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                            <li class="list-inline-item active">
+                                                <a href="index_Main.php">Home</a>
+                                            </li>
+                                            <li class="list-inline-item seprate">
+                                                <span>/</span>
+                                            </li>
+                                            <li class="list-inline-item">Cerificate of Residency</li>
+                                        </ul>
+                                    </div>
+                                    <!-- <button class="au-btn au-btn-icon au-btn--green">
+                                        <i class="zmdi zmdi-plus"></i>add item</button> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+                <!-- code here -->
+                <div class="clearance">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col mt-3">
+                                        <h1>header</h1>
+                                    </div>
+                                </div>
+                                <hr>
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu ullamcorper diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut gravida eleifend purus, non molestie eros tempor ut. Pellentesque viverra dignissim malesuada. Quisque dignissim, est at faucibus porttitor, velit lacus gravida magna, eget dignissim ligula nisl in est. Aliquam sed tortor vitae lorem vulputate egestas. Nulla nec erat eros.
+
+                                Cras sit amet risus nisl. Etiam eget nisl non sapien porta consequat ut in tellus. Pellentesque ipsum ligula, condimentum et feugiat ut, viverra ut eros. Curabitur porta justo leo, vitae tincidunt nibh tempor nec. Vivamus quis ex luctus, lobortis leo vel, varius orci. Nullam consequat arcu in elit efficitur semper. In mollis hendrerit blandit.
+
+                                Morbi vel rutrum mauris. In non sapien bibendum purus viverra pretium. Quisque nulla orci, varius sed pulvinar nec, volutpat ultrices eros. Ut non enim vel justo consectetur vehicula. Nulla ultricies neque id mauris cursus, in elementum turpis congue. Cras varius molestie hendrerit. Mauris tincidunt tellus quam, non aliquet mauris scelerisque ut. Curabitur neque metus, aliquet ut ligula ut, fringilla finibus lectus. Pellentesque porttitor eu metus sit amet scelerisque.
+
+                                Sed eu sem ac ipsum gravida maximus. Vestibulum sit amet vestibulum elit. Sed volutpat purus metus, at malesuada nunc semper id. Integer cursus aliquet viverra. Sed pulvinar blandit ante eu efficitur. Duis quis vestibulum leo. Suspendisse potenti. Etiam ut orci quis enim euismod interdum ut ac justo. Vestibulum ac erat in ex vehicula accumsan. Nunc nulla magna, molestie sit amet erat ut, aliquam placerat elit. Suspendisse volutpat quis orci non volutpat. Aenean elementum dolor ut erat elementum consequat. In tempor, massa et vulputate molestie, quam arcu hendrerit sapien, id ornare ante nibh vel arcu.
+
+                                Suspendisse potenti. Ut sit amet tincidunt enim. Nam a cursus eros. In hac habitasse platea dictumst. Fusce et sem vitae diam pulvinar fermentum et eu augue. Sed a imperdiet ante, vitae venenatis mi. Maecenas bibendum sed leo vitae varius. Donec nec eros at ipsum lobortis maximus. Maecenas at dui a tortor viverra laoreet. Sed metus orci, efficitur at enim nec, eleifend lacinia ligula. Aliquam elementum tellus id lacus facilisis varius et ut augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In ut luctus libero. Vivamus fringilla eu elit vitae gravida. Sed dictum ac mauris in viverra.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<?php
+    include 'footer.php';
+
+?>
